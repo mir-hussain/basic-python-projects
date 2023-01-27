b265a19f96aee7d4d5027947aa850a8fc8c00568
mir-hussain <mirhussainmurtaza@gmail.com>
2023-01-27 21:12:27 +0600

    avg height using for loop added

--- avarage-height/script.py
+++ avarage-height/script.py
@@ -1,10 +1,25 @@
 print("Average height calculator.")
 
-height_string = input(
-    "Enter the height in cm. separate the measurements by comma. (eg. 176, 145, 164) \n")
+# Not using For loop
 
-height_list = height_string.replace(" ", "").split(",")
+height_list = input(
+    "Enter the height in cm. separate the measurements by comma. (eg. 176, 145, 164) \n").replace(" ", "").split(",")
 
 avg = round(sum(list(map(int, height_list))) / len(height_list), 2)
 
 print(f"The average hight is {avg} cm.")
+
+
+# Using for loop
+
+# height_list = input(
+#     "Enter the height in cm. separate the measurements by comma. (eg. 176, 145, 164) \n").replace(" ", "").split(",")
+
+# sum = 0
+
+# for height in height_list:
+#     sum += int(height)
+
+# avg = sum / len(height_list)
+
+# print(f"The average hight is {avg} cm.")
---
+++ avarage-height/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+avg = round(sum(list(map(int, height_list))) / len(height_list), 2)
+
+print(f"The average hight is {avg} cm.")
