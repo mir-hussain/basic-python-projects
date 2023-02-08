48716d2676f485ece778f42c88c8bd59a26dc3fa
mir-hussain <mirhussainmurtaza@gmail.com>
2023-02-09 02:59:51 +0600

    random walk added

---
+++ random-walk/script.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+from random import randint, choice
+
+
+pen = Turtle()
+screen = Screen()
+
+pen.shape("circle")
+pen.width(10)
+pen.speed(250)
+color_list = ["red", "green", "orange", "blue", "black", "purple", "gray"]
+
+
+for number in range(500):
+    pen.color(choice(color_list))
+    pen.right(45 * randint(0, 10))
+    pen.forward(50)
+
+screen.exitonclick()
